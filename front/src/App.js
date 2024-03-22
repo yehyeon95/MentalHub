d3105d24066a5acb3a326e73c88c4ec57e38afdb
yehyeon95 <si7531@naver.com>
2024-03-22 14:31:51 +0900

    writeComponent

--- front/src/App.js
+++ front/src/App.js
@@ -8,7 +8,7 @@ import EditName from './pages/editName';
 import EditPassword from './pages/editPassword';
 import MyPage from './pages/myPage';
 import Board from './pages/board';
-
+import Write from './pages/write';
 //App.js 는 index.js에서 로드하는 파일로 각 컴포넌트에 대한 루트 즉 라우팅을 관리합니다.
 function App () {
     return (
@@ -22,6 +22,7 @@ function App () {
                 <Route path="/editPassword" element={<EditPassword />} />
                 <Route path="/editProfile" element={<EditProfile />} />
                 <Route path="/myPage" element={<MyPage />} />
+                <Route path="/write" element={<Write />} />
 
 
             </Routes>
---
+++ front/src/components/writeComponent.js
@@ -0,0 +1,31 @@
+import {Editor as Writer} from '@toast-ui/react-editor';
+
+function WriteComponent () {
+    
+    return (
+        <div>
+            <div className="container">
+                <div className="mb-4 editor-wrapper">
+                    <h1>글을 작성해주세요</h1>
+                    <Writer
+                        initialValue="내용을 입력하세요" // 초기값 설정
+                        previewStyle="vertical" // 미리보기 스타일 설정
+                        height="500px" // 에디터 높이 설정
+                        initialEditType="markdown" // 초기 에디터 타입 설정 (markdown 또는 wysiwyg)
+                        useCommandShortcut={true} // 단축키 사용 설정
+                        //onChange={onChangeHandle}
+                        toolbarItems={[
+                            ['heading', 'bold', 'strike'],
+                            ['hr', 'quote'],
+                            ['ul', 'ol', 'task'],
+                            ['table', 'image', 'link'],
+                            ['code', 'codeblock'],
+                        ]}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WriteComponent
